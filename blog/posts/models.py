2b386aa9d220397711c2f6de86bed968907f7111
jwilsontt <jwilsontt@gmail.com>
2016-05-16 22:17:33 -0400

    Create comments.

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 
 from django.conf import settings
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from django.core.urlresolvers import reverse
 from django.db.models.signals import pre_save
@@ -13,6 +14,7 @@ from markdown_deux import markdown
 from comments.models import Comment
 
 # Create your models here.
+
 
 class PostManager(models.Manager):
     def active(self, *args, **kwargs):
@@ -59,6 +61,12 @@ class Post(models.Model):
         instance = self
         qs = Comment.objects.filter_by_instance(instance)
         return qs
+
+    @property
+    def get_content_type(self):
+        instance = self
+        content_type = ContentType.objects.get_for_model(instance.__class__)
+        return content_type
 
 def create_slug(instance, new_slug=None):
     slug = slugify(instance.title)
