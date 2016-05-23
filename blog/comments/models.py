3362fa7fc95199bed7c0d874a1672a86a44e1ff0
jwilsontt <jwilsontt@gmail.com>
2016-05-23 16:47:57 -0400

    Added comment reply

--- blog/comments/models.py
+++ blog/comments/models.py
@@ -7,10 +7,14 @@ from django.contrib.contenttypes.models import ContentType
 
 # Create your models here.
 class CommentManager(models.Manager):
+    def all(self):
+        qs = super(CommentManager, self).filter(parent=None)
+        return qs
+
     def filter_by_instance(self, instance):
         content_type = ContentType.objects.get_for_model(instance.__class__)
         obj_id = instance.id
-        qs = super(CommentManager, self).filter(content_type=content_type, object_id=obj_id)
+        qs = super(CommentManager, self).filter(content_type=content_type, object_id=obj_id).filter(parent=None)
         return qs
 
 class Comment(models.Model):
@@ -18,13 +22,27 @@ class Comment(models.Model):
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey('content_type', 'object_id')
+    parent      = models.ForeignKey("self", null=True, blank=True)
     content     = models.TextField()
     timestamp   = models.DateTimeField(auto_now_add=True)
 
+
     objects = CommentManager()
+
+    class Meta:
+        ordering = ['-timestamp']
 
     def __unicode__(self):
         return str(self.user.username)
 
     def __str__(self):
         return str(self.user.username)
+
+    def children(self): #replies
+        return Comment.objects.filter(parent=self)
+
+    @property
+    def is_parent(self):
+        if self.parent is not None:
+            return False
+        return True
